b787cfe261e8de8fcc6b2fbe8d61bf5b47003f21
Bas <bas@local.com>
2015-06-15 20:15:21 +0200

    Nieuwe chrome extensie (spel versie 2 na gesprek Frank)

--- ChromeExtension/js/content_injector.js
+++ ChromeExtension/js/content_injector.js
@@ -2,19 +2,15 @@ var app = window.app = {};
 
 (function() {
 	
-	app.articleElement = undefined;
-	app.title = undefined;
-	app.article = undefined;
-	
-	app.titleElement = undefined;
-	app.articleElement = undefined;
-	
-	app.gameEngine = undefined;
-	app.HowManyWords = 3;
-	
-	app.wordObject = undefined;
-	
 	app.gameStarted = false;
+	
+	app.gameInterval;
+	app.fastGameInterval;
+	
+	app.timeCounter = 0;
+	
+	app.playingWord;
+	app.wordsPlayed = [];
 	
 	var fn = {
 		injectPlayButton: function() {
@@ -22,7 +18,7 @@ var app = window.app = {};
 			var introElement = $('p.intro');	
 			if (introElement.length > 0) {
 				
-				var elementToAdd = $('<div id="playTheGameButton">Ben je altijd op de hoogte van het laatste nieuws?<span>Speel ons nieuwe spel en test je kennis!</span></div>');
+				var elementToAdd = $('<div id="playTheGameButton">Ben je altijd op de hoogte van het laatste nieuws?<span>Speel ons nieuwe spel en test je snelheid!</span></div>');
 					
 				introElement.before(elementToAdd);
 				elementToAdd.on('click', function() {
@@ -34,166 +30,187 @@ var app = window.app = {};
 					
 				});
 				
-				// debug
-				//fn.startTheGame();
+			}
+			
+		},
+		addWord: function(element, word) {
+			
+			if (fn.filterWord(word).length > 2) {
+			
+				var elementToAdd = $("<span class=\"gameWord\">");
+				element.append(elementToAdd.text(word));
+				elementToAdd.after("<span> </span>");
+				
+				elementToAdd.attr('data-word', fn.filterWord(word).toLowerCase());
+				
+			} else {
+				
+				var elementToAdd = $("<span>");
+				element.append(elementToAdd.text(word));
+				elementToAdd.after("<span> </span>");				
 				
 			}
+				
+		},
+		filterWord: function(word) {
+			
+			word = word.replace('.', '');
+			word = word.replace(',', '');
+			word = word.replace(':', '');
+			//word = word.replace('\'', '');
+			//word = word.replace('\'', '');
+			word = word.replace('"', '');
+			word = word.replace('"', '');
+			word = word.replace('*', '');
+		
+			return $.trim(word);	
+			
+		},
+		randomFrom: function(array) {
+			
+			return Math.floor(Math.random() * array.length);
+			
+		},
+		fastGameInterval: function() {
+			
+			var allWords = $('.gameWord');			
+			allWords.removeClass('fastSelected');
+			
+			for(var i = 0; i < parseInt(allWords.length / 3); i++) {
+				
+				allWords.eq(fn.randomFrom(allWords)).addClass('fastSelected');
+				
+			}
+			
+		},
+		gameInterval: function() {
+			
+			app.timeCounter += 1;	
+			
+			if (app.timeCounter > 1) {
+			
+				$('#gameOverlay').find('.left .seconds').text('Verstreken tijd: ' + app.timeCounter + ' seconden');	
+				
+			} else {
+				
+				$('#gameOverlay').find('.left .seconds').text('Verstreken tijd: ' + app.timeCounter + ' seconde');	
+				
+			}
+			
+		},
+		getRandomWordFromText: function() {
+			
+			var allWords = $('.gameWord');			
+			
+			return allWords.eq(fn.randomFrom(allWords)).text();
+								
+		},
+		playNewWord: function(word) {
+			
+			app.timeCounter = 0;			
+			app.playingWord = word;
+			
+			$('#gameOverlay').find('.left .wordButton').text(fn.filterWord(word).toLowerCase());
+			
+			var allWords = $('.gameWord');	
+			allWords.removeClass('goodAnswer');
+			allWords.off('click');
+			
+			allWords.each(function(index) {
+				
+				var thisWord = $(this).attr('data-word');
+				
+				if (thisWord == fn.filterWord(word).toLowerCase()) {				
+					$(this).addClass('goodAnswer');					
+				}
+				
+			});
+			
+			$('body').find('.gameWord.goodAnswer').on('click', function() {
+				
+				app.wordsPlayed.push([app.playingWord, app.timeCounter]);
+
+				var countWords = app.wordsPlayed.length;
+				var countTotalScore = 0;
+				
+				$.each(app.wordsPlayed, function(i, v) {
+	
+					countTotalScore += v[1];
+				
+				});
+				
+				var countScore = (countTotalScore / countWords).toFixed(2);				
+				$('.yourScore').text(countScore + ' seconden per woord. (' + countWords + ' woorden)');
+				
+				if (countWords < 10) {
+				
+					fn.playNewWord(fn.getRandomWordFromText());
+				
+				} else {
+					
+					var allWords = $('.gameWord');	
+					allWords.removeClass('goodAnswer');
+					allWords.off('click');
+					
+					allWords.removeClass('fastSelected');
+					
+					clearInterval(app.fastGameInterval);
+					clearInterval(app.gameInterval);
+					
+					$('#gameOverlay').find('.left').html('');
+					
+					alert('Je hebt het spel uitgespeeld!\nScore: ' + countScore);
+					
+				}
+				
+			});
+			
+		},
+		replaceAll: function(find, replace, str) {
+			
+			return str.split(find).join(replace);
 			
 		},
 		startTheGame: function() {
 			
+			var articleTitleElement = $('h1#articleDetailTitle');
+			var detailContentElement = $('#detail_content');
+			
 			// Wrap all title words in a span
-			var words = app.titleElement.text().split(" ");
-			app.titleElement.empty();
+			var words = articleTitleElement.text().split(" ");
+			articleTitleElement.empty();
 			$.each(words, function(i, v) {
-				var elementToAdd = $("<span class=\"gameWord\">");
-				app.titleElement.append(elementToAdd.text(v));
-				elementToAdd.after("<span> </span>");
+				fn.addWord(articleTitleElement, v);
 			});
 
 			// Wrap all articale words in a span
-			app.articleElement.find('p').each(function(index) {
+			detailContentElement.find('p').each(function(index) {
 				var articleElement = $(this);
-				var words = $(this).text().split(" ");	
+				articleElement.find('strong').remove();
+				var text = fn.replaceAll('.', '. ', articleElement.text());		
+				var words = text.split(" ");	
 				articleElement.empty();
 				$.each(words, function(i, v) {
-					var elementToAdd = $("<span class=\"gameWord\">");
-				    articleElement.append(elementToAdd.text(v));
-				    elementToAdd.after("<span> </span>");
+					fn.addWord(articleElement, v);
 				});
 			});
 			
-			app.gameEngine = new GameEngine();			
-			app.wordObject = app.gameEngine.run(app.titleElement, app.articleElement, app.HowManyWords);
-	
-			fn.addPlayables();
+			$('body').prepend('<div id="gameOverlay"><div class="holder"><div class="left"><div class="text">Zoek het woord:</div><div class="wordButton"></div><div class="text seconds"></div></div><div class="right"><div class="text">Huidige score:</div><div class="wordButton yourScore"><i>Nog geen</i></div></div></div></div>')
 			
-		},
-		replaceWordInArticle: function(word, alternatives) {
+			fn.gameInterval();
+			fn.fastGameInterval()
 			
-			$('span.gameWord').each(function(index) {
-				
-				var wordInSpan = $(this).text().toLowerCase();
-				
-				if (wordInSpan == word) {
-
-					$(this).data('word', wordInSpan.toLowerCase());
-					$(this).data('words', alternatives);
-					$(this).text('???').addClass('playable').on('click', function() {
+			app.gameInterval = setInterval(fn.gameInterval, 1000);
+			app.fastGameInterval = setInterval(fn.fastGameInterval, 100);
+			
+			fn.playNewWord(fn.getRandomWordFromText());
 						
-						fn.showOptionSelect($(this), word, alternatives);
-						
-					});
-					
-				}
-				
-			});
-			
-		},
-		showOptionSelect: function(wordElement, word, alternatives) {
-			
-			var overlayElement = $('<div id="gameOverlay"></div>');			
-			$('body').prepend(overlayElement);
-			
-			var blockElement = $('<div class="gameBlock"><h1>Wat is het juiste woord?</h1><div class="words"></div></div>');		
-			overlayElement.append(blockElement);
-			
-			var allWords = alternatives.concat();
-			allWords.push(word);
-
-			$.each(allWords, function(index, value) {
-
-				var wordElementToAdd = $('<div class="gameWordButton">' + value.toLowerCase() + '</div>'); 
-				blockElement.find('.words').append(wordElementToAdd);
-				
-				wordElementToAdd.on('click', function() {
-					
-					var clickedWord = $(this).text().toLowerCase();	
-					
-					$("span.gameWord.playable").each(function(index) {
-					
-						var thisWordElement = $(this);
-						
-						var thisWord = thisWordElement.data('word').toLowerCase();
-						
-						if (thisWord == clickedWord || thisWordElement.data('words').indexOf(clickedWord) > -1) {
-							
-							thisWordElement.text(clickedWord);
-							thisWordElement.removeClass('correct');
-							if (clickedWord == thisWord) {			
-								thisWordElement.addClass('correct');				
-							}										
-							
-						}
-					
-					});
-								
-					overlayElement.fadeOut(function() {
-						
-						overlayElement.remove();
-						
-					});
-					
-				});
-
-			});
-			
-			blockElement.fadeIn();			
-			wordElement.text(word);
-			
-		},
-		addPlayables: function() {
-			
-			if (typeof app.wordObject != "undefined") {				
-				$.each(app.wordObject, function(index, value) {
-
-					fn.replaceWordInArticle(value.word, value.alternatives);
-
-				});
-			}
-			
-		},
-		readArticle: function() {
-
-			if (typeof app.articleElement != "undefined") {
-				
-				var articleTitleElement = $('h1#articleDetailTitle');				
-				if (articleTitleElement.length > 0) {
-					
-					app.titleElement = articleTitleElement;
-					
-					app.title = articleTitleElement.text();
-					
-				}
-				
-				var detailContentElement = $('#detail_content')		
-				if (detailContentElement.find('p').length > 0) {
-					
-					app.articleElement = detailContentElement;
-					
-					app.article = "";				
-					detailContentElement.find('p').each(function(index) {
-						
-						app.article += $(this).text() + "\n";
-						
-					});
-					
-				}		
-				
-				if (typeof app.title != "undefined" && typeof app.article != "undefined") {
-					
-					fn.injectPlayButton();
-					
-				}
-						
-			}
-			
 		},
 		init: function() {
 
 			app.articleElement = $('.article.art_detail');
 			if (app.articleElement.length > 0) {		
-				fn.readArticle();				
+				fn.injectPlayButton();				
 			}
 					
 		}
